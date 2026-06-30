1051d0d2b8d52af956c97888699f6fab40b8ea63
VividMarionette <vividlinux@gmail.com>
2026-06-30 10:03:33 +0200

    Login.py

---
+++ Projects/Login_state/Login.py
@@ -0,0 +1,11 @@
+username = "Git"
+password = "Hub"
+
+user = input("Enter your username: ")
+pw = input("Enter your password: ")
+
+if user == username and pw == password:
+  print("ACCESS GRANTED")
+
+else:
+  print("ACCESS DENIED!")
