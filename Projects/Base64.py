93b507cec6686f20a52a0730294cf36896f7f357
VividMarionette <vividlinux@gmail.com>
2026-06-30 08:58:48 +0200

    deobfuscate_payload.py

--- Projects/Base64.py
+++ Projects/Base64.py
@@ -1,1 +1,12 @@
+import base64
 
+base64_link = """
+#Put the link you recognize as base64 to decode the link
+"""
+
+audio_data = base64.b64decode
+  (cow_base64)
+with open("cow_base64.mp3", "wb") as f:
+  f.write(audio_data)
+
+print("Audio file successfully created as a cow audio mp3")
